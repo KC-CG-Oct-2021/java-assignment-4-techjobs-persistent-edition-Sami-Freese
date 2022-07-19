908b46822a66d23ac00a0b436229b6201cdbbb2a
Sami Freese <sami.freese@gmail.com>
2022-07-18 20:22:27 -0500

    Assignment Completed, Autograder Passing

--- queries.sql
+++ queries.sql
@@ -9,14 +9,20 @@
 
 -- Part 2: Test it with SQL
 
--- SELECT *
--- FROM employer
--- WHERE (location = "St. Louis City");
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
 
 -- Part 3: Test it with SQL
 
---DROP TABLE job
+DROP TABLE job;
 
 
 -- Part 4: Test it with SQL
+
+SELECT *
+FROM skill
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
